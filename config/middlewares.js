37a75a7eb3e1ca0009168e3386735529586a223c
raymund manzanares <raymund.manzanares@portable.com.au>
2022-10-19 08:20:08 +1100

    Strapi with S3 bucket

---
+++ config/middlewares.js
@@ -0,0 +1,38 @@
+const { env } = require('@strapi/utils/lib');
+
+module.exports = [
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            env('AWS_BUCKET') + '.s3.' + env('AWS_REGION') +  '.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            env('AWS_BUCKET') + '.s3.' + env('AWS_REGION') +  '.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::favicon',
+  'strapi::public',
+];
